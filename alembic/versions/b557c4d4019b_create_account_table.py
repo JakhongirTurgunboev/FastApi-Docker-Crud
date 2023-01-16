ceac61203d8408d776d75e815707325b7adb8ff5
Asus <jahongirbaxtiyorovich17@gmail.com>
2023-01-16 21:35:25 +0500

    first commit

---
+++ alembic/versions/b557c4d4019b_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: b557c4d4019b
+Revises: 61055af0da89
+Create Date: 2023-01-16 21:04:13.334555
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b557c4d4019b'
+down_revision = '61055af0da89'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
